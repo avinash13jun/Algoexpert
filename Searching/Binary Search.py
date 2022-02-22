8d22b956c7f44f82daf77e2421a66bc293824670
Avinash Kumar <akumar40@stevens.edu>
2022-02-21 19:32:04 -0500

    Update Binary Search.py

--- Searching/Binary Search.py
+++ Searching/Binary Search.py
@@ -4,4 +4,25 @@ Created on Sat Feb 19 12:03:18 2022
 
 @author: Avinash Kumar
 """
+import math
 
+def binarySearch(array, target):
+    array.sort()
+    sIdx = 0
+    lIdx = len(array) - 1
+    mid = math.floor(len(array) / 2)
+    while sIdx <= lIdx:
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:
+            lIdx = mid - 1
+            mid = math.floor((sIdx + lIdx) / 2)
+        else:
+            sIdx = mid + 1
+            mid = math.floor((sIdx + lIdx) / 2)
+    return -1
+    
+array = [0, 1, 21, 33, 45, 45, 61, 71, 72, 73]
+target = 33
+res = binarySearch(array, target)
+print(res)
