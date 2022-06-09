237538f97aead5c2074565ddac02f09047bbc4ec
Avinash Kumar <akumar40@stevens.edu>
2022-06-09 16:55:16 -0400

    Array medium problem

---
+++ Arrays/Three Number Sum.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  9 15:11:05 2022
+
+@author: Avinash Kumar
+"""
+
+def threeNumberSum(array, targetSum):
+    array.sort()
+    res = []
+    for i in range(len(array) - 2):
+        sIdx = i+1
+        lIdx = len(array) - 1
+        while lIdx > sIdx:
+            value = array[i] + array[sIdx] + array[lIdx]
+            if value > targetSum:
+                lIdx-=1
+            elif value < targetSum:
+                sIdx+=1
+            else:
+                res.append([array[i], array[sIdx], array[lIdx]])
+                sIdx+=1
+                lIdx-=1
+    return res
+
+array = [12, 3, 1, 2, -6, 5, -8, 6]
+targetSum = 0
+res = threeNumberSum(array, targetSum)
+print(res)
