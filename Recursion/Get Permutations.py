71203def9b1d02752e132ad5faea18ff933f201e
Avinash Kumar <akumar40@stevens.edu>
2022-07-28 00:32:16 -0400

    Update Get Permutations.py

--- Recursion/Get Permutations.py
+++ Recursion/Get Permutations.py
@@ -1,14 +1,27 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jun 23 17:26:26 2022
+Created on Wed Jul 27 23:05:08 2022
 
 @author: Avinash Kumar
 """
 
 def getPermutations(array):
-    # Write your code here.
-    pass
+    permutations = []
+    permutationHelper(0, array, permutations)
+    return permutations
 
-test = [1, 2, 3]
-res = getPermutations(test)
+def permutationHelper(i, array, permutations):
+    if i == len(array) - 1:
+        permutations.append(array[:])
+    else:
+        for j in range(i, len(array)):
+            swap(array, i, j)
+            permutationHelper(i+1, array, permutations)
+            swap(array, i, j)
+
+def swap(array, i, j):
+    array[i], array[j] = array[j], array[i]
+    
+array = [1, 2, 3]
+res = getPermutations(array)
 print(res)
